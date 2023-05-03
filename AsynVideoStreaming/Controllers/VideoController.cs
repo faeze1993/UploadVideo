e8f650ad32cb9040ead3331c94bbf013016c0872
faeze ghanbary <faezeghanbary72@gmail.com>
2023-05-03 11:32:48 +0330

    .

---
+++ AsynVideoStreaming/Controllers/VideoController.cs
@@ -0,0 +1,51 @@
+﻿using AsynVideoStreaming.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace AsynVideoStreaming.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/video")]
+    [ApiController]
+    public class VideoController : ApiController
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public VideoController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        [Microsoft.AspNetCore.Mvc.HttpGet("{ext}/{fileName}")]
+        public HttpResponseMessage Get(string ext, string fileName)
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            string videoPath = string.Format("~/Videos/{0}.{1}", fileName, ext);
+            videoPath = "D:\\AsynVideoStreaming\\AsynVideoStreaming\\Videos\\sample.mp4";//Path.Combine(webRootPath, videoPath);
+
+            if (File.Exists(videoPath))
+            {
+                FileInfo fi = new FileInfo(videoPath);
+                var video = new VideoStream(videoPath);
+
+                var response = Request.CreateResponse();
+
+                response.Content = new PushStreamContent((Action<Stream, HttpContent, TransportContext>)video.WriteToStream,
+                new MediaTypeHeaderValue("video/" + ext));
+
+                response.Content.Headers.Add("Content-Disposition", "attachment;filename=" + fi.Name.Replace(" ", ""));
+                response.Content.Headers.Add("Content-Length", video.FileLength.ToString());// در صورتی که این Header‌‌ها را تعریف نکنید سایز فایل دریافتی و مدت زمان آن نامعلوم خواهد بود که تجربه کاربری خوبی بدست نمی‌دهد.
+
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
--- AsynVideoStreaming/Models/VideoStream.cs
+++ AsynVideoStreaming/Models/VideoStream.cs
@@ -1,5 +1,50 @@
-﻿namespace AsynVideoStreaming.Models;
+﻿using System.Net;
+
+namespace AsynVideoStreaming.Models;
 
 public class VideoStream
 {
+    private readonly string _filename;
+    private long _contentLength;
+
+    public long FileLength => _contentLength;
+
+    public VideoStream(string videoPath)
+    {
+        _filename = videoPath;
+        //FileShare.Read
+        //باعث می‌شود فایل مورد نظر هنگام باز شدن قفل نشود و برای پروسه‌های دیگر قابل دسترسی باشد.
+        using (var video = File.Open(videoPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+        {
+            _contentLength = video.Length;
+        }
+    }
+
+    public async void WriteToStream(Stream outputStream, HttpContent content, TransportContext context)
+    {
+        try
+        {
+            var buffer = new byte[65536];// 64KB
+            using (var video = File.Open(_filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                var length = (int)video.Length;
+                var bytesRead = 1;
+
+                while (length > 0 && bytesRead>0)
+                {
+                    bytesRead = video.Read(buffer, 0, Math.Min(length, buffer.Length));
+                    await outputStream.WriteAsync(buffer, 0, bytesRead);
+                    length -= bytesRead;
+                }
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        finally
+        {
+            outputStream.Close();
+        }
+    }
 }
